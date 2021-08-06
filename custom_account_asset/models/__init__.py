e050253ba70f656a72e89df0e670543485953596
deyanarajib182 <mochamad.deyana@dwidasa.com>
2021-08-06 13:38:06 +0700

    Initial commit

--- custom_account_asset/models/__init__.py
+++ custom_account_asset/models/__init__.py
@@ -1,6 +1,5 @@
 from . import account_asset
 from . import account_move
 from . import account_asset_correction
-from . import account_account
 from . import res_company
 from . import res_config_settings
