454c057f6ffd2db926620a47c91757a602caf2ab
deyanarajib182 <mochamad.deyana@dwidasa.com>
2021-08-08 19:09:27 +0700

    Initial commit

--- l10n_id_mpn_chart/__manifest__.py
+++ l10n_id_mpn_chart/__manifest__.py
@@ -9,10 +9,9 @@
     'summary': "MPN Chart Template",
     'website': 'https://www.dwidasa.com',
     'email': 'community@dwidasa.com',
-    'depends': ['account', 'base_iban', 'base_vat'],
+    'depends': ['account', 'base_iban', 'base_vat', 'l10n_generic_coa'],
     'license': 'LGPL-3',
     'data': [
-        'l10n_generic_coa',
         'data/account_account_type_data.xml',
         'data/account_chart_template_data.xml',
         'data/account.account.template.csv',
