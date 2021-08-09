3909edab6439221a20678067ec51fe9cb77f26b3
deyanarajib182 <mochamad.deyana@dwidasa.com>
2021-08-09 15:34:51 +0700

    Initial commit

--- custom_account_asset/models/res_company.py
+++
@@ -1,7 +0,0 @@
-from odoo import models, fields
-
-
-class ResCompany(models.Model):
-    _inherit = 'res.company'
-
-    compute_asset_based_opening = fields.Boolean(string='Compute Asset based Opening')
--- custom_website_portal/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from . import website
-from . import website_menu
-from . import sale
-from . import hr_expense
-from . import hr_expense_sheet
